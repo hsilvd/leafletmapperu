81e4c9129b46f318d1827126d1f6838ecb871299
hsilvd <124374584+hsilvd@users.noreply.github.com>
2023-02-16 21:45:37 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1eX_iu8J90fsesitHfu8NvViCu3Q8COdSFeoTG0Af9fk/edit#gid=0';
 
 //insert your own Google Sheets API Key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfk87WhY';
+var googleApiKey = 'AIzaSyC-h4eRQb0VW3uex3SFYdqZeThrOezwusc';
