eb7b43a8c3b9c69e43939c57f9f7d9e4741737d9
hsilvd <124374584+hsilvd@users.noreply.github.com>
2023-02-16 20:49:31 -0500

    added google spreadsheet

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1eX_iu8J90fsesitHfu8NvViCu3Q8COdSFeoTG0Af9fk/edit#gid=0';
+
+//insert your own Google Sheets API Key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfk87WhY';
